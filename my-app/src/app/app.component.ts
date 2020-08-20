1b19410fad0a54ba5601ac4fb7048d69dd8a183a
VeronicaChenyuk <beresneva.veronika@mail.ru>
2020-08-20 17:47:20 +0300

    2:24

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,16 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormsModule } from "@angular/forms";
+import { Observable } from "rxjs";
+import { map, startWith } from "rxjs/operators";
+import { ValueConverter } from '@angular/compiler/src/render3/view/template';
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  constructor(private snackBar: MatSnackBar) { }
+  openSnackBar(message, action) {
+    let snackBarref = this.snackBar.open(message, action, { duration: 2000 });
+
+    snackBarref.afterDismissed().subscribe(() => {
+      console.log('The snackbar was dismissed');
+
+    })
+
+    snackBarref.onAction().subscribe(() => {
+      console.log('The sanckbar action was triggered!');
+
+    })
+  }
+  openCustomSnackBar() {
+    this.snackBar.openFromComponent(CustomSnackBarComponent, { duration: 2000 }
+    )
+  }
+
+  minDate = new Date();
+  maxDate = new Date(2020, 3, 5);
+
+  dateFilter = date => {
+    const day = date.getDay();
+    return day !== 0 && day !== 6;
+  }
+
+  options: string[] = ['Angular', 'React', 'Vue'];
+  objectOptions = [
+    { name: 'Angular' },
+    { name: 'Angular Material' },
+    { name: 'React' },
+    { name: 'Vue' },
+  ];
+
+  myControl = new FormControl();
+  filteredOptions: Observable<string[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    )
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLocaleLowerCase();
+    return this.options.filter(option => option.toLocaleLowerCase().includes(filterValue))
+  }
+
+  slectedValue: string
   opened = false;
   title = 'my-app';
   notifications = 0;
   showSpinner = false;
+
+  displayFn(subject) {
+    return subject ? subject.name : undefined
+  }
 
   logChange(index) {
     console.log(index);
@@ -28,3 +88,9 @@ export class AppComponent {
     }, 5000)
   }
 }
+
+@Component({
+  selector: 'custom-snackbar',
+  template: `<span style='color:orange'>Custom Snackbar</span>`
+})
+export class CustomSnackBarComponent { }
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent,CustomSnackBarComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from "./material/material.module";
 import { MatIconModule } from "@angular/material/icon";
@@ -10,7 +10,7 @@ import { MatBadgeModule } from "@angular/material/badge";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from "@angular/material/sidenav";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatMenuModule } from "@angular/material/menu";
 import { MatListModule } from "@angular/material/list";
 import { MatDividerModule } from '@angular/material/divider';
@@ -19,11 +19,24 @@ import { MatExpansionModule } from "@angular/material/expansion";
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from "@angular/material/tabs";
 import { MatStepperModule } from "@angular/material/stepper";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomSnackBarComponent
   ],
+  entryComponents:[CustomSnackBarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -42,7 +55,18 @@ import { MatStepperModule } from "@angular/material/stepper";
     MatExpansionModule,
     MatCardModule,
     MatTabsModule,
-    MatStepperModule
+    MatStepperModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatTooltipModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
